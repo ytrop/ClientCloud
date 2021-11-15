5e55ea10ce1c7c34653196503ff5e9decc3e3363
ytrop <62332814+ytrop@users.noreply.github.com>
2021-11-15 14:36:29 +0100

    Add files via upload
    Commit

---
+++ prueba-clientemicro/src/main/java/com/example/prueba/clientemicro/PruebaClientemicroApplication.java
@@ -0,0 +1,14 @@
+package com.example.prueba.clientemicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PruebaClientemicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaClientemicroApplication.class, args);
+	}
+
+}
